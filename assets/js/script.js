1b724c40bef7f14af4f705aff3a6c4ea0e5c15ac
bruno <bpires.siqueira@gmail.com>
2022-10-23 17:21:49 -0300

    funcao cifra-de-cesar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@
 
 // ALFABETO CIFRADO DE 4 SALTOS
 //     => EFGHIJKLMNOPQRSTUVWXYZABCD
+
+function cifraCesar(palavra, passo) {
+    let alfabeto = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let p = palavra.split('').map((e) => (alfabeto.indexOf(e) + passo) % alfabeto.length);
+    return p.map((e) => alfabeto[e]).join('');
+}
