2269b4237231b993ea6bb2965b9661dc38dada6c
bruno <bpires.siqueira@gmail.com>
2022-10-23 17:23:23 -0300

    setando os elementos

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,10 @@ function cifraCesar(palavra, passo) {
     let p = palavra.split('').map((e) => (alfabeto.indexOf(e) + passo) % alfabeto.length);
     return p.map((e) => alfabeto[e]).join('');
 }
+
+// SETANDO OS ELEMENTOS
+
+const msga = document.querySelector('[data-msg-a]');
+const msgb = document.querySelector('[data-msg-b]');
+const btn = document.querySelector('button');
+const select = document.querySelector('#select');
